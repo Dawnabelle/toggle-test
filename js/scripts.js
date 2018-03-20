5791d3ec1e9f8fc908230bbe1546e13903a159ce
Brenda Franco and Dawn Mott <dawn@>
2018-03-20 11:40:51 -0700

    Add buttons to change color scheme.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
     $("button#dark").click(function(){
+      $(".wrapper").removeClass("light-scheme");
+      $(".profile").removeClass("light-scheme");
       $(".wrapper").addClass("dark-scheme");
       $(".profile").addClass("dark-scheme");
     });
+
+    $("button#light").click(function() {
+      $(".wrapper").removeClass("dark-scheme");
+      $(".profile").removeClass("dark-scheme");
+      $(".wrapper").addClass("light-scheme");
+      $(".profile").addClass("light-scheme");
+  });
 });
